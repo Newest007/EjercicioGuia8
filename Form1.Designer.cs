3a5cdd691816dd0eb43e7a30c16498294da2a124
Arian <adilsonarian@outlook.com>
2022-04-06 20:09:38 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Ejercicio_Guía_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.tbHeap = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.tbHeap.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(402, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 66);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Montículos\r\nHeap Maximizante";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnOrdenar);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.btnLimpiar);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.txtNumero);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(161, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(668, 104);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controles";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Location = new System.Drawing.Point(540, 37);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(99, 44);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.Location = new System.Drawing.Point(303, 37);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(99, 44);
+            this.btnLimpiar.TabIndex = 2;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.Location = new System.Drawing.Point(184, 37);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(99, 44);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(32, 44);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(136, 30);
+            this.txtNumero.TabIndex = 0;
+            // 
+            // tbHeap
+            // 
+            this.tbHeap.Controls.Add(this.tabPage1);
+            this.tbHeap.Location = new System.Drawing.Point(12, 200);
+            this.tbHeap.Name = "tbHeap";
+            this.tbHeap.SelectedIndex = 0;
+            this.tbHeap.Size = new System.Drawing.Size(990, 457);
+            this.tbHeap.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 28);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(982, 425);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Heap";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage1_Paint);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrdenar.Location = new System.Drawing.Point(421, 37);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(99, 44);
+            this.btnOrdenar.TabIndex = 5;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 669);
+            this.Controls.Add(this.tbHeap);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Montículos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tbHeap.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TabControl tbHeap;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnOrdenar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_Guía_8
+{
+    public partial class Form1 : Form
+    {
+
+        int xo, yo, tam; //Variables para el valor inicial de x, y y del tamaño
+        bool ec = false; //Bandera que más adelante se usara
+        bool estado = false; //Otra bandera más adeltante usada
+        int n = 0, i = 1;
+        //i es e nivel
+        //n son los numeros de nodos
+        int[] arreglo_numeros; //Arreglo para los números ingresados
+        Button[] Arreglo; //Arreglo de boton para simular los valores ingresados
+
+
+        //==========================================================================================//
+        //                 Método que intercambiara valores de heap de números                      //
+        public void HPN()
+        {
+            int temp; //Variable temporal
+            int x = arreglo_numeros.Length; //Longitud del arreglo de los valores ingresados
+
+            for (int i = (x - 1); i >= 1; i--) //Desde un valor menos de la longitud total se decrementa
+            {
+                intercambio(ref Arreglo, i, 1); //Se hace el intercambio
+                temp = arreglo_numeros[i]; //El elemento i del arreglo pasa a la variable temporal
+                arreglo_numeros[i] = arreglo_numeros[1]; //El elemento 1 pasa a la posición i
+                arreglo_numeros[1] = temp; //El que estaba en temporal pasa a la posición 1
+                x--;
+            }
+        }
+
+        //==========================================================================================//
+        //                        Método para hacer el intercambio                                  //
+        public void intercambio(ref Button[] botones, int a, int b)
+        {
+            string temp = botones[a].Text; //Dejando los valores en una variable temporal
+
+            Point pa = botones[a].Location; //Sacando la ubicación de a
+            Point pb = botones[b].Location; //Sacando la ubicación de b
+
+            int diferenciaX = Math.Abs(pa.X - pb.X); //Sacamos la distancia entre sus X´s
+            int diferenciaY = Math.Abs(pa.Y - pb.Y); //Sacamos la distancia entre sus Y´s
+
+            int x = 10;
+            int y = 10;
+            int t = 70;
+
+            while (y != diferenciaY + 10) //Mientras no se llegue a la posición esperada en y
+            {
+                Thread.Sleep(t); //Suspendiendo durante 70 milisegundos
+                botones[a].Location += new Size(0, -10); //Moviendo "a" -10
+                botones[b].Location += new Size(0, +10); //Moviendo "b" +10
+                y += 10;
+            }
+
+            while (x != diferenciaX - diferenciaX % 10 + 10) //Mientras no lluegue a la posición esperada en x
+            {
+                if (pa.X < pb.X) //Si x de a es menor que x de b
+                {
+                    Thread.Sleep(t); //Suspendiendo durante 70 milisegundos
+                    botones[a].Location += new Size(+10, 0); //Movemos +10 a "a"
+                    botones[b].Location += new Size(-10, 0); //Movemos -10 a "b"
+                    x += 10;
+                }
+                else
+                {
+                    Thread.Sleep(t);
+                    botones[a].Location += new Size(-10, 0);
+                    botones[b].Location += new Size(+10, 0);
+                    x += 10;
+                }
+
+            }
+
+            botones[a].Text = botones[b].Text; //El valor de b se muestra en a
+            botones[b].Text = temp; //El valor temporal almacenado se mostrará en b
+            botones[b].Location = pb; //Nuevo pb, se almacenará en su ubicación
+            botones[a].Location = pa; //Nuevo pa, se va a almacenar en su ubicación
+            estado = true;
+
+            tabPage1.Refresh(); //Para refrescar el tabPage
+
+        }
+        //==========================================================================================//
+        //                        Métodos para dibujar en el tab pages                              //
+
+        //                        Método para dibujar las ramas                                     //
+        public void dibujar_Ramas(ref Button[] botones, ref TabPage tb, PaintEventArgs e)
+        {
+            Pen lapiz = new Pen(Color.Gray, 1.5f);
+            Graphics g;
+            g = e.Graphics;
+
+            for (int j = 1; j < Arreglo.Length; j++) //Para todos los elementos del arreglo
+            {
+                if (Arreglo[(2 * j)] != null) //Mientras el arreglo no esté vacio
+                    g.DrawLine(lapiz, Arreglo[j].Location.X, Arreglo[j].Location.Y + 20, Arreglo[(2 * j)].Location.X + 20, Arreglo[(2 * j)].Location.Y);
+                if (Arreglo[(2 * j) + 1] != null) //Mientra no haya solo un elemento
+                    g.DrawLine(lapiz, Arreglo[j].Location.X + 40, Arreglo[j].Location.Y + 20, Arreglo[(2 * j) + 1].Location.X + 20, Arreglo[(2 * j) + 1].Location.Y);
+            }
+
+        }
+
+        //                        Método para dibujar el arreglo                                    //
+        public void Dibuja_Arreglo(ref Button[] botones, ref TabPage tb)
+        {
+            for (int j = 1; j < botones.Length; j++) 
+            {
+                tb.Controls.Add(this.Arreglo[j]);
+            }
+
+        }
+
+        //==========================================================================================//
+        //                           Método para un Heap Máximizante                                //
+        public void Heap_Num()
+        {
+            ec = true; //Pasando la bandera true
+
+            int x = Arreglo.Length;
+
+            for (int i = ((x) / 2); i > 0; i--) 
+                Max_Num(arreglo_numeros, x, i, ref Arreglo);
+
+        }
+
+        //Para un número minimo en heap
+        public void Max_Num(int[] a, int x, int indice, ref Button[] botones)
+        {
+            int izquierdo = (indice * 2);
+            int derecho = (indice * 2) + 1;
+            int mayor = 0;
+
+            if (izquierdo < x && a[izquierdo] > a[indice])
+            {
+                mayor = izquierdo;
+            }
+            else
+            {
+                mayor = indice;
+            }
+
+            if (derecho < x && a[derecho] > a[mayor])
+            {
+                mayor = derecho;
+            }
+
+            if(mayor != indice) // Si el menor es distinto del indice
+            {
+                int temp = a[indice]; //Valor indice será el temporal
+                a[indice] = a[mayor]; //El menor se almacena en la posición del indice
+                a[mayor] = temp; //El temporal se almacena en el menor
+
+                intercambio(ref Arreglo, mayor, indice); //Llamando al método intercambio
+                Max_Num(a, x, mayor, ref botones); //Llamada recursiva a Max_num
+
+            }
+
+        }
+        //==========================================================================================//
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num = int.Parse(txtNumero.Text);
+
+                Array.Resize<int>(ref arreglo_numeros, i + 1); //Incrementamos el arreglo en base al nuevo valor ingresado
+                arreglo_numeros[i] = num; //Asignamos ese valor a la posición i del arreglo
+                Array.Resize<Button>(ref Arreglo, i + 1); //Incrementamos el arreglo de botones
+                Arreglo[i] = new Button(); //Creando un nuevo botón i
+                Arreglo[i].Text = arreglo_numeros[i].ToString(); //El texto del botón será el valor ingresado en la posición i 
+                Arreglo[i].Height = 50; //Altura del botón
+                Arreglo[i].Width = 50; //Ancho del botón
+                Arreglo[i].BackColor = Color.FromArgb(222,17,84);
+                Arreglo[i].ForeColor = Color.White;
+                Arreglo[i].Location = new Point(xo, yo) + new Size(-20, 0);
+
+                //Para dibuajr el árbol y crear los niveles se hace
+                if ((i + 1) == Math.Pow(2, n + 1)) 
+                {
+                    n++; //Se incrementa n
+                    tam = tam / 2; //Para colocar el nodo justo en medio del tab page
+                    xo = tam; //Xo tendrá el mismo valor que el de tam
+                    yo += 60; //Incrementando la Y en 60 para que el siguiente nivel se dibuje 60 espacios más abajo
+                }
+                else
+                {
+                    xo += (2 * tam); //Si en cierto caso no se cumple lo anterior el nodo se móvera en x
+                }
+
+                i++; //Incrementando i
+                estado = true;
+                ec = false;
+                tabPage1.Refresh(); //Refrescando el tabPage
+                txtNumero.Clear();
+                txtNumero.Focus();
+
+            }
+            catch { MessageBox.Show("Valor no Válido");}
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            //Limiando la pantalla y reiniciando los valores iniciales
+            n = 0;
+            i = 1;
+            tam = tabPage1.Width / 2;
+            xo = tam;
+            yo = 20;
+            tabPage1.Controls.Clear();
+            tabPage1.Refresh();
+            Array.Resize<int>(ref arreglo_numeros, 1);
+            Array.Resize<Button>(ref Arreglo, 1);
+        }
+
+        private void tabPage1_Paint(object sender, PaintEventArgs e)
+        {
+            if(estado) //Si el estado es verdadero
+            {
+                try
+                {
+                    Dibuja_Arreglo(ref Arreglo, ref tabPage1); //Dibuja el arreglo
+                    dibujar_Ramas(ref Arreglo, ref tabPage1, e); //Dibujar ramas
+                }
+                catch { }
+
+                estado = false; //Pasa el estado a falso
+            }
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (i == 1)
+            {
+                MessageBox.Show("No hay elementos que ordenar");
+            }
+
+            else
+            {
+                btnAgregar.Enabled = false;
+                btnLimpiar.Enabled = false;
+                btnOrdenar.Enabled = false;
+                this.Cursor = Cursors.WaitCursor;
+
+                if (!ec)
+                {
+                    Heap_Num(); //Llamando el heap num
+                }
+                else
+                {
+                    HPN(); //Llamando el hpn
+                }
+
+                //Se habilita todo de nuevo
+                btnAgregar.Enabled = true;
+                btnLimpiar.Enabled = true;
+                btnOrdenar.Enabled = true;
+                this.Cursor = Cursors.Default;
+
+            }
+        }
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            tam = tabPage1.Width / 2; //tam será la mitad del ancho del tabpage
+            xo = tam; //El valor inicial de x será la mitad del ancho del tabpage
+            yo = 20; //El valor inicial en y será de 20
+            txtNumero.Focus(); //Para hacer un focus
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
